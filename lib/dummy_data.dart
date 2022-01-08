e21a0073473cd616ecd810a537b44f4f24db01df
yazeedwong <yazeedwong@gmail.com>
2022-01-08 11:42:27 +0800

    ok

---
+++ lib/dummy_data.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import './models/category.dart';
+
+const DUMMY_CATEGORIES = const [
+  Category(
+    id: 'c1',
+    title: 'italian',
+    color: Colors.purple,
+  ),
+  Category(
+    id: 'c2',
+    title: 'Quick & E',
+    color: Colors.red,
+  ),
+  Category(
+    id: 'c3',
+    title: 'Hamburgers',
+    color: Colors.orange,
+  ),
+  Category(
+    id: 'c4',
+    title: 'german',
+    color: Colors.amberAccent,
+  ),
+  Category(
+    id: 'c5',
+    title: 'light & lovely',
+    color: Colors.blue,
+  ),
+];
